cd1dce4b29d163c9ac318b1800c50fb922599b57
Michael Myers <michael.myers@wustl.edu>
2026-05-04 08:58:12 -0500

    added run_pipeline.jl stub

---
+++ bin/run_pipeline.jl
@@ -0,0 +1,10 @@
+
+using DumerilPatois
+ 
+@info "concatenate stage"
+# call concatenate_pages, write build/intro.md and build/entries.md
+ 
+@info "parse stage (not yet implemented)"
+ 
+@info "emit stage (not yet implemented)"
+
